2e106f2a052cb1be0d59aebe377ee862ffadfe5f
BartlomiejRasztabiga <bartlomiej.rasztabiga.official@gmail.com>
2017-06-16 18:47:16 +0200

    Initial commit

---
+++ src/main/java/HalDeserializer.java
@@ -0,0 +1,12 @@
+import java.net.URL;
+
+public class HalDeserializer {
+    private HalDeserializer() {
+
+    }
+
+    public static class Builder {
+        private URL baseUrl;
+        
+    }
+}
---
+++ src/test/java/DeserializationTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+public class DeserializationTest {
+
+    @Test
+    public void deserialize_test() throws Exception {
+
+    }
+}
