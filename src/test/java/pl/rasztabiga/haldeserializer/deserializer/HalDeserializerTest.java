1ed35d1b326fa6c156629e9355d0a53d026c43a9
BartlomiejRasztabiga <bartlomiej.rasztabiga.official@gmail.com>
2017-06-30 18:12:28 +0200

    WIP

---
+++ src/test/java/pl/rasztabiga/haldeserializer/deserializer/HalDeserializerTest.java
@@ -0,0 +1,14 @@
+package pl.rasztabiga.haldeserializer.deserializer;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HalDeserializerTest {
+
+    @Test
+    public void buildInstanceWithMalformedURL_test() throws Exception {
+        
+    }
+
+}
