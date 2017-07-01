32f63eb62a5a59b2a0e8b25677b1601072f3c0e4
BartlomiejRasztabiga <bartlomiej.rasztabiga.official@gmail.com>
2017-07-01 12:46:24 +0200

    ResourceBundleTest: WIP

--- src/test/java/pl/rasztabiga/haldeserializer/deserializer/ResourceBundleTest.java
+++ src/test/java/pl/rasztabiga/haldeserializer/deserializer/ResourceBundleTest.java
@@ -1,15 +1,18 @@
 package pl.rasztabiga.haldeserializer.deserializer;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import pl.rasztabiga.haldeserializer.json.JSONException;
 import pl.rasztabiga.haldeserializer.json.JSONObject;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -22,8 +25,6 @@ public class ResourceBundleTest {
 
     @Mock
     private HalParser mockParser;
-
-    private TwoFieldsClass twoFieldsObject = new TwoFieldsClass("First", "Second");
 
     @Before
     public void initMocks() {
@@ -55,23 +56,126 @@ public class ResourceBundleTest {
         assertEquals("secondField", fieldsList.get(1).getName());
     }
 
-    @Test
-    public void retrieveResourceWithTwoFieldsClassAndPlainJson_test() throws Exception {
-        ResourceBundle resourceBundle = new ResourceBundle(new JSONObject(), twoFieldsClass);
+    // FIXME: 01.07.2017  Integration test
+    @Test(expected = JSONException.class)
+    public void retrieveResourceFromPlainJsonWithoutLinks_throwsJSONException_test() throws Exception { //TODO Support plain json in future
+        String twoFieldsObjectInJson = "{\n" +
+                "\t\"firstField\": \"something\",\n" +
+                "\t\"secondField\": \"something else\"\n" +
+                "}";
+        JSONObject jsonObject = new JSONObject(twoFieldsObjectInJson);
 
-        //TODO
+        ResourceBundle resourceBundle = new ResourceBundle(jsonObject, twoFieldsClass);
 
+        Field targetClassField = ResourceBundle.class.getDeclaredField("targetClass");
+        targetClassField.setAccessible(true);
+        targetClassField.set(resourceBundle, twoFieldsClass);
+
+        Method retrieveResourceMethod = ResourceBundle.class.getDeclaredMethod("retrieveResource", JSONObject.class);
+        retrieveResourceMethod.setAccessible(true);
+
+        try {
+            retrieveResourceMethod.invoke(resourceBundle, jsonObject);
+        } catch (InvocationTargetException e) {
+            if (e.getCause() instanceof JSONException) {
+                throw new JSONException(e.getCause());
+            }
+        }
     }
 
-    private static class NoFieldsClass { }
+    // FIXME: 01.07.2017  Integration test
+    @Test
+    public void retrieveResourceFromHalJsonWithLinks_test() throws Exception {
+        String twoFieldsObjectInJson = "{\n" +
+                "\t\"firstField\": \"something\",\n" +
+                "\t\"secondField\": \"something else\",\n" +
+                "\t\"_links\": {\n" +
+                "\t\t\n" +
+                "\t}\n" +
+                "}";
+        JSONObject jsonObject = new JSONObject(twoFieldsObjectInJson);
+
+        ResourceBundle resourceBundle = new ResourceBundle(jsonObject, twoFieldsClass);
+
+        Method retrieveResourceMethod = ResourceBundle.class.getDeclaredMethod("retrieveResource", JSONObject.class);
+        retrieveResourceMethod.setAccessible(true);
+
+        Resource resource = (Resource) retrieveResourceMethod.invoke(resourceBundle, jsonObject);
+        TwoFieldsClass object = (TwoFieldsClass) resource.getContent();
+
+        assertEquals("something", object.getFirstField());
+        assertEquals("something else", object.getSecondField());
+    }
+
+    @Test
+    public void retrieveResourceFromHalJsonWithLinks_unitTest() throws Exception {
+        String twoFieldsObjectInJson = "{\n" +
+                "\t\"firstField\": \"something\",\n" +
+                "\t\"secondField\": \"something else\",\n" +
+                "\t\"_links\": {\n" +
+                "\t\t\n" +
+                "\t}\n" +
+                "}";
+        JSONObject jsonObject = new JSONObject(twoFieldsObjectInJson);
+
+        ResourceBundle resourceBundle = new ResourceBundle(jsonObject, twoFieldsClass);
+
+        Field parserField = ResourceBundle.class.getDeclaredField("parser");
+        parserField.setAccessible(true);
+        parserField.set(resourceBundle, mockParser);
+
+        when(mockParser.retrieveLinks(any(JSONObject.class))).thenReturn(new ArrayList<HalLink>()); //Return empty hal links list, not needed
+        when(mockParser.parseResource(any(JSONObject.class), anyList(), any(Class.class))).thenReturn(new TwoFieldsClass("something", "something else"));
+
+        Method retrieveResourceMethod = ResourceBundle.class.getDeclaredMethod("retrieveResource", JSONObject.class);
+        retrieveResourceMethod.setAccessible(true);
+
+        Resource resource = (Resource) retrieveResourceMethod.invoke(resourceBundle, jsonObject);
+        TwoFieldsClass object = (TwoFieldsClass) resource.getContent();
+
+        assertEquals("something", object.getFirstField());
+        assertEquals("something else", object.getSecondField());
+    }
+
+    @Ignore
+    @Test
+    public void retrieveResourcesFromHalJsonWithEmbeddedAndLinks_test() throws Exception {
+        //TODO RetrieveResource
+        fail("Not yet implemented");
+    }
+
+
+
+
+    private static class NoFieldsClass {
+    }
 
     private static class TwoFieldsClass {
         private String firstField;
         private String secondField;
 
+        public TwoFieldsClass() {
+        }
+
         public TwoFieldsClass(String firstField, String secondField) {
             this.firstField = firstField;
             this.secondField = secondField;
+        }
+
+        public String getFirstField() {
+            return firstField;
+        }
+
+        public String getSecondField() {
+            return secondField;
+        }
+
+        @Override
+        public String toString() {
+            return "TwoFieldsClass{" +
+                    "firstField='" + firstField + '\'' +
+                    ", secondField='" + secondField + '\'' +
+                    '}';
         }
     }
 }
